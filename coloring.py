428fb464fc2fbb6b1f52e1c91aa8ff2e946a08a0
deligon86 <95563202+deligon86@users.noreply.github.com>
2023-09-22 10:31:20 +0300

    Update coloring.py
    Optimized the code abit

--- coloring.py
+++ coloring.py
@@ -6,61 +6,7 @@ from matplotlib.colors import rgb2hex
 def convert_to_hex(color:list):
 
     return rgb2hex(c=color)
-
-"""
-class BackgroundDetector:
-
-    def __init__(self, path):
-        self.img = cv2.imread(path, 1)
-        self.manual_count = {}
-        self.w, self.h, self.channels = self.img.shape
-        self.total_pixels = self.w * self.h
-
-    def count(self):
-        for y in range(0, self.h):
-            for x in range(0, self.w):
-                RGB = (self.img[x, y, 2], self.img[x, y, 1], self.img[x, y, 0])
-                if RGB in self.manual_count:
-                    self.manual_count[RGB] += 1
-                else:
-                    self.manual_count[RGB] = 1
-
-    def average_count(self):
-        red = 0
-        green = 0
-        blue = 0
-        sample = 10
-        for top in range(0, sample):
-            red += self.number_counter[top][0][0]
-            green += self.number_counter[top][0][1]
-            blue += self.number_counter[top][0][2]
-
-        average_red = red / sample
-        average_green = green / sample
-        average_blue = blue / sample
-
-        print("Average for top ten RGB", average_red, average_green, average_blue)
-
-    def twenty_most_common(self):
-        self.count()
-        self.number_counter = Counter(self.manual_count).most_common(20)
-        '''for rgb, value in self.number_counter:
-            print(rgb, value, ((float(value)/self.total_pixels))*100)'''
-
-    def detect(self):
-        self.twenty_most_common()
-        ''' self.percentage_of_first = (
-            float(self.number_counter[0][1]) / self.total_pixels
-        )
-        print(self.percentage_of_first)
-        if self.percentage_of_first > 0.5:
-            print("Background color", self.number_counter[0][0])
-            return self.number_counter[0][0]
-        else:
-            self.average_count()'''
-
-        return self.number_counter[0][0]
-"""
+
 
 
 class PilDetector:
@@ -113,6 +59,7 @@ class PilDetector:
         return color
 
     def __transform_color(self, color, hex=False):
+        """ The default color is rgb change hex to True to transform to hex value """
         if isinstance(color, tuple):
             color = list(color)
         if len(color) > 3:
